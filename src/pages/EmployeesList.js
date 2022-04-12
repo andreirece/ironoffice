b2157a144972cc66fa32e010d2ef0ae7499eef37
Cintia Mendes <cintiamendesx@gmail.com>
2022-04-12 06:02:18 -0300

    employees list

--- src/pages/EmployeesList.js
+++ src/pages/EmployeesList.js
@@ -2,8 +2,7 @@ import axios from "axios";
 import React from "react";
 import { useEffect, useState } from "react";
 import { styled } from '@mui/material/styles';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
+
 
 
 
@@ -13,9 +12,10 @@ function EmployeesList() {
 
     const Img = styled('img')({
         margin: 'auto',
-        display: 'block',
-        maxWidth: '100%',
-        maxHeight: '100%',
+        display: 'inline',
+        width: '100px',
+        height: '100px',
+        borderRadius: '50%',
       });
 
   
@@ -26,48 +26,86 @@ function EmployeesList() {
         setEmployees(...[response.data]);
       }).catch ((err)=> {console.error(err)});
     }, []);
-    console.log(employees)
-  
+
     if (!employees) return null;
-    
-  
-    return (
-        <div>
 
-     <Paper
-      sx={{
-        p: 2,
-        margin: 'auto',
-        maxWidth: 500,
-        flexGrow: 1,
-        backgroundColor: (theme) =>
-          theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-      }}
-    >
-        
 
-        {employees.map((currentEmployees) => {
+    function handleInput(e) {
+      const inputValue = e.target.value; 
+      setEmployees(searchTable(inputValue));
+     }
 
-          return (
+  function searchTable(value) {
+ const filteredData = [];
 
-            <Grid container spacing={2}>    
- 
-          <tr key={currentEmployees._id}>
-            <th style={{width: "100px"}}>
-           
-            <Img src={currentEmployees.image} alt={currentEmployees.name}/>
-            </th>
-            <th>{currentEmployees.name}</th>
-            <th>{currentEmployees.area}</th>
-          </tr>
-          
+ if (value.length === 0) {
+   return employees; 
+ }
 
-            </Grid>
-        )})}
-        </Paper>
-      </div>
-    );
+ for (let i = 0; i < employees.length; ++i) {
+  const newValue = value.toLowerCase();
+
+  const user = setEmployees[i].usuario.toLowerCase();
+
+  if (user.includes(newValue)) {
+    filteredData.push(setEmployees[i]);
+  }
+ }
+ return filteredData;
 }
+
+/*------------------------------------------------------------*/
+ function sorted() {
+    const sortedEmployees = [...employees];
+    sortedEmployees.sort((a, b) => {
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name > b.name) {
+      return 1;
+    }
+    return 0;
+  });}
+
+  console.log(employees);
+/*------------------------------------------------------------*/
+   
+  return (
+    <table>
+      <thead>
+      <input
+       className="form-input"
+       onChange={handleInput} 
+       id="input-table"
+       placeholder="Ache um Usuario"
+      />
+        <tr>
+          <th>Colaborador</th>
+          <th> <button type="button" onClick={() => sorted('Nome')}> Nome </button></th>
+          <th>Contato</th>
+          <th>Função</th>
+          <th>Área</th>
+        </tr>
+      </thead>
+      <tbody>
+        {employees.map((currentEmployees) => (
+
+        <tr key={currentEmployees._id}>
+<Img src={currentEmployees.image} alt={currentEmployees.name}/>
+
+            <td>{currentEmployees.name}
+</td>
+            <td>{currentEmployees.contact}
+</td>
+           <td>{currentEmployees.function}
+</td>
+            <td>{currentEmployees.area}
+</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );}
 
 export default EmployeesList;
 
